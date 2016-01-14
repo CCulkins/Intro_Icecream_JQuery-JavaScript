6eef3dfae0487334b89f3722cdd61ca30b62757d
Cassandra Culkins and Chris Kuiper <cassy.culkins@gmail.com>
2016-01-13 16:19:24 -0800

    update javascript to make application work

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   $("form").submit(function(event){
-    var blanks =["flavor1", "flavor2", "flavor3", "flavor4", "flavor5"];
-    blanks.forEach(function(blank) {
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
+    var labels =["flavor1", "flavor2", "flavor3", "flavor4", "flavor5"];
+
+    labels.forEach(function(label) {
+      var userInput = $("input#" + label).val();
+      $("." + label).text(userInput);
     });
+
+    var favoriteFlavor = $("input#flavor1").val();
+    alert('I love ' + favoriteFlavor + ', too!');
+
     $(".favorite").show();
     event.preventDefault();
   });
